03d2282fbe93cfe7e7aab6cf65ce1f3a574fb74b
Vinayak Kumawat <iamvinayakkt@gmail.com>
2024-04-23 22:39:17 +0530

    added total visitors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import Image from 'next/image'
 import { menu } from '@/components/constants'
 import Link from 'next/link'
+import { CounterAPI } from "counterapi";
+
+const counter = new CounterAPI();
+let totalVisitors: any = null;
+
+counter.up("circon24.com", "circon24").then((res) => {
+  totalVisitors = res.Count as number;
+})
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col justify-center md:items-center gap-4 bg-black text-white px-4 py-12 sm:px-8 md:px-16'>
+    <footer className='flex flex-col justify-center md:items-center gap-4 bg-black text-white px-4 py-8 sm:px-8 md:px-16'>
+      <div className='flex justify-start items-center gap-4 mb-4'>
+        <p className='text-sm font-semibold'>Total Visitors: <span className='p-1 border-white'>{totalVisitors}</span></p>
+      </div>
       <div className='flex flex-col md:flex-row items-start gap-2 md:gap-16 mb-4'>
         {menu.map((item) => (
           <Link key={item.id} href={item.link} target={item.newTab ? '_blank' : '_self'} className='font-medium hover:underline transition-all'>{item.label}</Link>
