ba17bc8770d562d8c2e021e3b373ba8e485accaf
Vinayak Kumawat <iamvinayakkt@gmail.com>
2024-04-25 11:36:26 +0530

    speaker mobile version update

--- components/ui/photoCard.tsx
+++ components/ui/photoCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const PhotoCard = ({profileimage, fullName, des}: {profileimage: string, fullName: string, des: string}) => {
     return (
-        <div className='py-16 flex flex-col justify-center items-center'>
+        <div className='py-2 md:py-16 flex flex-col justify-center items-center'>
             <Image className='w-48 h-48 sm:w-52 sm:h-52 md:w-60 md:h-60 lg:w-72 lg:h-72 object-cover rounded-full border-[6px] border-secondaryBg' src={profileimage} alt='Image' width={256} height={256} priority />
             <h3 className='text-xl md:text-2xl lg:text-3xl font-semibold mt-8'>{fullName}</h3>
             <p className='text-base lg:text-lg mt-2'>{des}</p>
