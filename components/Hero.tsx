aa7a3b816b526675dcd4a1ec10395e28d9ed3541
InternetMadeCoder <anonymous210705@gmail.com>
2025-08-21 00:23:57 +0530

    change hero section bg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import { Belanosima } from "next/font/google";
 
 const belanosima = Belanosima({
-    subsets: ["latin"],
-    weight: ['400', '600'],
+  subsets: ["latin"],
+  weight: ["400", "600"],
 });
 
 const Hero = () => {
+  return (
+    <div className="relative overflow-hidden h-[90vh]">
+      <Image
+        src="/images/bg/hero-section.jpg"
+        alt="Hero Background"
+        fill
+        className="object-cover z-0"
+        priority
+      />
 
-    return (
-        <div className='relative overflow-hidden'>
-            <video autoPlay loop muted className='w-full h-screen object-cover z-0 -mt-16'>
-                <source src='/videos/hero-video.mp4' type='video/mp4' height={500} />
-            </video>
-
-            <div className='absolute top-0 left-0 w-full h-full bg-black opacity-30'></div>
-            <div className='px-4 text-center absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center'>
-                <div className='-mt-80 md:-mt-24 sm:px-16'>
-                    <Image className='w-[700px]' src={'/images/graphics/hero-banner.png'} alt='CirCon-24' width={1024} height={512} />
-                </div>
-            </div>
-
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
+      <div className="px-4 text-center absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+        <div className="-mt-48 md:-mt-16 sm:px-16">
+          <Image
+            className="w-[700px]"
+            src={"/images/graphics/hero-banner.png"}
+            alt="CirCon-24"
+            width={1024}
+            height={512}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
