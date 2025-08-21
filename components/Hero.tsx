291172f9c81cf6707b88299209c66cb94d2009e4
InternetMadeCoder <anonymous210705@gmail.com>
2025-08-21 13:13:23 +0530

    add video to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,13 +14,17 @@ const Hero = () => {
 
   return (
     <div className="relative overflow-hidden h-[90vh]">
-      <Image
+      {/* <Image
         src="/images/bg/hero-section.jpg"
         alt="Hero Background"
         fill
         className="object-cover z-0"
         priority
-      />
+      /> */}
+
+      <video autoPlay loop muted className='w-full h-screen object-cover z-0 -mt-16'>
+                <source src='/videos/hero-video.mp4' type='video/mp4' height={500} />
+      </video>
 
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
       <div className="px-4 text-center absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
