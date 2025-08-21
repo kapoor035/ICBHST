f342f3cdb9436622c1b62d1624d55fe032f0a507
InternetMadeCoder <anonymous210705@gmail.com>
2025-08-21 15:53:03 +0530

    update organizing committee

--- components/CommitteeCard.tsx
+++ components/CommitteeCard.tsx
@@ -1,17 +1,25 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const CommitteeCard = ({ name, des, role, picture }: any) => {
   return (
     <div>
-        <div className='flex flex-col justify-center items-center p-4'>
-            <Image className='w-40 h-40 object-cover rounded-full border-[4px] border-secondaryBg' src={picture} alt={name} width={256} height={256} />
-            <h4 className='bg-secondaryBg text-white px-2 py-1 font-bold -mt-5'>{name}</h4>
-            <p>{role}</p>
-            <p>{des}</p>
-        </div>
+      <div className="flex flex-col justify-center items-center p-4">
+        <Image
+          className="w-48 h-56 object-cover border-[4px] border-secondaryBg"
+          src={picture}
+          alt={name}
+          width={256}
+          height={256}
+        />
+        <h4 className="bg-secondaryBg text-white px-2 py-1 font-bold -mt-5">
+          {name}
+        </h4>
+        <p>{role}</p>
+        <p>{des}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CommitteeCard
+export default CommitteeCard;
